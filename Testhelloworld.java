c3e362c71acd812deb741e42d54d1a5a785cb49f
Amirtha <30748438+amirtha17@users.noreply.github.com>
2022-09-13 03:16:52 -0700

    Update Testhelloworld.java

--- Testhelloworld.java
+++ Testhelloworld.java
@@ -1,15 +1,20 @@
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class Testhelloworld {
 	
-	
+	helloworld object = new helloworld();
 	@Test
 	public void testResult() {
-		helloworld ob = new helloworld();
-		String output = ob.Result();
+		
+		String output = object.Display();
 		assertEquals("Hello, World",output);
+	}
+	public static void main(String args[]) {
+		Testhelloworld object1 = new Testhelloworld();
+		object1.testResult();
 	}
 
 }
