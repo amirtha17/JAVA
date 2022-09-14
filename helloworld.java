e66d89a726abba19433a697e65ebfae60de9bf78
Amirtha <30748438+amirtha17@users.noreply.github.com>
2022-09-13 23:39:34 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,17 +1,11 @@
 
-public class helloworld 
+public class helloworld
 {
-	public String Display() {
-		String str = "Hello, World";
-		return str;
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
-	
-		public static void main(String args[]) 
-		{
-			helloworld obj = new helloworld();
-		  String str1 = obj.Display();
-		  System.out.println(str1);
-		   
-		}
-	
+	public String Result()
+	{
+	   return "Hello, World";
+	}
 }
