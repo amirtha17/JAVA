95a1211975c3769a8e1ab78d44635ebbd11fba86
Amirtha <30748438+amirtha17@users.noreply.github.com>
2022-09-14 04:11:50 -0700

    Add files via upload

---
+++ TestHelloworld.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+class TestHelloworld {
+	
+	Helloworld object = new Helloworld();
+	@Test
+	public void testResult() {
+		
+		String output = object.Display();
+		assertEquals("Hello Subhashri",output);
+	}
+	public static void main(String args[]) {
+		TestHelloworld object1 = new TestHelloworld();
+		object1.testResult();
+	}
+
+}
