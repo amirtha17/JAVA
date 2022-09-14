3edb0a0f17065d6689ac979af50772e9d8d932b3
Amirtha <30748438+amirtha17@users.noreply.github.com>
2022-09-14 04:11:20 -0700

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,16 @@
+public class Helloworld 
+{
+	public String Display() {
+		String str = "Hello Subhashri";
+		return str;
+	}
+	
+		public static void main(String args[]) 
+		{
+			Helloworld obj = new Helloworld();
+		  String str1 = obj.Display();
+		  System.out.println(str1);
+		   
+		}
+	
+}
